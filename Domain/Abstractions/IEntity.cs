6ee3f612fdf7aa3d31bda0521dd8a626026e9ed0
Ksyusha7650 <froganddog771@gmail.com>
2025-08-28 23:14:31 +0300

    2.1 Подготовка инфраструктуры для сущностей

---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+public interface IEntity
+{
+    
+}
---
+++ Domain/Entity.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions;
+
+namespace Domain;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public required T Id { get; set; }
+}

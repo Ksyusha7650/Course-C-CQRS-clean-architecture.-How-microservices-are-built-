79f9cf3d719878479ef1e39918b45e4a16ae6911
Ksyusha7650 <froganddog771@gmail.com>
2025-09-07 18:59:33 +0300

    2.4-2.7 EF

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,7 @@
+using Infrastructure.Data.DataBaseContext;
+using Infrastructure.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqLiteConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    await app.InitializeDatabaseAsync();
 }
 
 app.UseHttpsRedirection();
---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.DataBaseContext
+{
+    public class ApplicationDbContext :DbContext, IApplicationDbContext
+    {
+
+        public DbSet<Topic> Topics => Set<Topic>();
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Topic>()
+                .Property(t => t.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => TopicId.Of(value)
+                );
+
+            modelBuilder.Entity<Topic>()
+                .OwnsOne(t => t.Location, loc =>
+                {
+                    loc.Property(l => l.City).HasColumnName("City");
+                    loc.Property(l => l.Street).HasColumnName("Street");
+                });
+        }
+
+    }
+}
